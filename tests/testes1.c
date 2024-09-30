a8b28a9914fd4168fdaa83cc0a8a510ee0d421b7
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2024-09-30 21:46:09 +0000

    Criado Tests Folder

---
+++ tests/testes1.c
@@ -0,0 +1,14 @@
+#include "/workspace/TP1_Confiabilidade/unity/unity.h"
+#include "/workspace/TP1_Confiabilidade/src/sort.h"
+
+extern int Counter;
+
+void setUp(void)
+{
+  /* This is run before EACH TEST */
+  Counter = 0x5a5a;
+}
+
+void tearDown(void)
+{
+}
